2f16166d476bb4cd4f8ea2d7f15ed0fda2111636
ssamarasam <samcsegct@gmail.com>
2023-11-06 20:17:40 +0530

    added tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 import BookingForm from "./components/BookingForm";
+import { BrowserRouter } from "react-router-dom";
 
 // test("renders learn react link", () => {
 //   render(<App />);
@@ -15,9 +16,19 @@ test("Renders the BookingForm heading", () => {
   expect(headingElement).toHaveTextContent("Book Now");
 });
 
-// test(" updateTimes and initializeTimes", () => {
-//   const availableTimes = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
-//   render(<BookingForm availableTimes={availableTimes} />);
-//   const headingElement = screen.getByTestId("reserve");
-//   expect(headingElement).toHaveTextContent("Book Now");
-// });
+test("intialize or updatetimes", () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const reserveButton = screen.getByTestId("reserve");
+  fireEvent.click(reserveButton);
+
+  const testTime = [];
+  userEvent.selectOptions(
+    screen.getByLabelText("Choose Time"),
+    screen.getByRole("option", { name: testTime })
+  );
+  expect(screen.getByRole("option", { name: testTime }).selected).toBe(true);
+});
